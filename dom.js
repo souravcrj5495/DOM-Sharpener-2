16eaae77b542b2bd6707119183a581c234246a82
souravcrj5495 <souravcrj1995@gmail.com>
2023-07-02 23:17:46 +0530

    creating nodes and modifying dom

--- dom.js
+++ dom.js
@@ -23,11 +23,43 @@
 // item2.style.display = 'none';
 
 //QuerySelectorAll
-var secondItem = document.querySelectorAll('li');
-secondItem[1].style.color = 'green';
+// var secondItem = document.querySelectorAll('li');
+// secondItem[1].style.color = 'green';
 
-var odd= document.querySelectorAll('li:nth-child(odd)');
+// var odd= document.querySelectorAll('li:nth-child(odd)');
 
-for (var i=0; i<odd.length; i++){
-    odd[i].style.backgroundColor = 'green';
-}
+// for (var i=0; i<odd.length; i++){
+//     odd[i].style.backgroundColor = 'green';
+// }
+
+//Create a div
+var newDiv = document.createElement('div');
+
+//Add class
+newDiv.className = 'hello';
+
+//Add id
+newDiv.id = 'hello1';
+
+//add attr
+newDiv.setAttribute('title', 'Hello Div');
+
+//create text node
+var newDivText = document.createTextNode('Hello');
+
+// Add text to div
+newDiv.appendChild(newDivText);
+
+var container = document.querySelector('header .container');
+
+var h1= document.querySelector('header h1');
+
+console.log(newDiv);
+
+// newDiv.style.fontSize = '30px';
+
+container.insertBefore(newDiv, h1);
+var items=document.querySelector('div #items' );
+console.log(items);
+var item1=document.querySelector('div .list-group-item');
+items.insertBefore(newDiv,item1);
